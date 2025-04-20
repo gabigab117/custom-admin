476926ea7c9b06ca51f5ac1415cce99b4a3e84c9
gabrieltrouve <gabrieltrouve5@gmail.com>
2025-04-20 08:22:17 +0200

    Add nested_admin to INSTALLED_APPS and implement admin interfaces for Product, Category, and SubCategory models

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+import nested_admin
 
-# Register your models here.
+from .models import Category, SubCategory, Product, ProductVariant, ProductVariantImage
+
+
+class ProductVariantImageInline(nested_admin.NestedTabularInline):
+    model = ProductVariantImage
+    extra = 1
+
+
+class ProductVariantInline(nested_admin.NestedStackedInline):
+    model = ProductVariant
+    extra = 1
+    inlines = [ProductVariantImageInline]
+
+
+@admin.register(Product)
+class ProductAdmin(nested_admin.NestedModelAdmin):
+    list_display = ["name", "subcategory"]
+    inlines = [ProductVariantInline]
+
+
+class SubCategoryInline(admin.StackedInline):
+    model = SubCategory
+    extra = 1
+    prepopulated_fields = {"slug": ("name", )}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    prepopulated_fields = {"slug": ("name", )}
+    inlines = [SubCategoryInline]
